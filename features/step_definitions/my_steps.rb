2113e37ad8bffa0c3cdd48b0191cd3d8fce7c9c5
kleer <kleer@kleer-csd-box>
2017-12-01 09:22:00 -0500

    Primer commit de lógica

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -1,1 +1,11 @@
+Dado(/^que inicie un juego$/) do
+	visit '/'
+end
 
+Entonces(/^debo ingresar "([^"]*)" como "([^"]*)"$/) do |letra, campoIntento|
+  fill_in(campoIntento, :with => letra)
+end
+
+Entonces(/^debo ver un boton para comparar la letra en la palabra$/) do
+  click_button("Probar")
+end
