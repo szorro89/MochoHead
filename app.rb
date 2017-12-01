8e80b67e65a05fc7500719356cfebeb49b5e1841
kleer <kleer@kleer-csd-box>
2017-12-01 14:14:57 -0500

    [WARNING] Ajuste parcial funcionalidad ingresarLetras

--- app.rb
+++ app.rb
@@ -7,12 +7,19 @@ get '/' do
 	generador = GeneradorPalabra.new
 	session['palabras']= generador.obtenerPalabra
 	session['vidas']= generador.obtenerVidas
+	session['letrasBuscadas'] = nil
 	erb :ahorcado
 end
 
 post '/ingresarLetra' do
-
 	ahorcado = Ahorcado.new session['palabras'][1]
-	session['palabras'][0]=ahorcado.validarLetra ["a"]
+	if session['letrasBuscadas'] == nil
+		letrasProbadas = ['']
+	else
+		letrasProbadas = session['letrasBuscadas']
+	end
+	letrasProbadas.push params['letraProbar']
+	session['letrasBuscadas'] = letrasProbadas
+	session['palabras'][0]=ahorcado.validarLetra letrasProbadas
 	erb :ahorcado
 end
